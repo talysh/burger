924c5fa255ea20e0b4924c16b9cee954ddb1cab1
talysh <taleh.aliyev@gmail.com>
2020-11-06 21:13:07 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+--DROP DATABASE IF EXISTS burgers_db
+
+CREATE DATABASE burgers_db
+USE burgers_db
+
+CREATE TABLE burgers {
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (256) NOT NULL,
+    devoured BOOLEAN default FALSE,
+    PRIMARY KEY (id)
+}
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db
+
+INSERT INTO bugers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO bugers (burger_name) VALUES ('The Hungry Sailor');
+INSERT INTO bugers (burger_name) VALUES ('Yummy Yumm Burger');
